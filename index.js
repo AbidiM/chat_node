6728cf3e1f57da9b209a0c1b2d488a44f60804d3
AbidiM <mohamed-abidi@live.fr>
2024-04-17 09:35:57 +0100

    Initial commit 2

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const app = require('express')()
-const http = require('http').createServer(app)
+// const app = require('express')()
+// const http = require('http').createServer(app)
 
-app.get('/', (req, res) => { res.send("Node Server is running") })
+// app.get('/', (req, res) => { res.send("Node Server is running") })
 
-//Socket Logic
-const socketio = require('socket.io')(http)
+// //Socket Logic
+// const socketio = require('socket.io')(http)
 
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data)
-    })
-})
+// socketio.on("connection", (userSocket) => {
+//     userSocket.on("send_message", (data) => {
+//         userSocket.broadcast.emit("receive_message", data)
+//     })
+// })
 
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat_message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat_message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 http.listen(process.env.PORT)
