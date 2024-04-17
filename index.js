2a7ef47eeca4fd1ed4da86dbecefd6c56b6417b8
AbidiM <mohamed-abidi@live.fr>
2024-04-17 11:46:44 +0100

    Initial commit 3

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-// const app = require('express')()
-// const http = require('http').createServer(app)
-
-// app.get('/', (req, res) => { res.send("Node Server is running") })
-
-// //Socket Logic
-// const socketio = require('socket.io')(http)
-
-// socketio.on("connection", (userSocket) => {
-//     userSocket.on("send_message", (data) => {
-//         userSocket.broadcast.emit("receive_message", data)
-//     })
-// })
-
-
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat_message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat_message', msg);
-  });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+
+//simple
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('chat_message', (msg) => {
+//         console.log('message: ' + msg);
+//         io.emit('chat_message', msg);
+//     });
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+// });
+
+//rooms
+io.on('connection', socket => {
+    //Get the chatID of the user and join in a room of the same chatID
+    chatID = socket.handshake.query.chatID
+
+    console.log('user ' + chatID + ' connected');
+    socket.join(chatID)
+
+    //Leave the room if the user closes the socket
+    socket.on('disconnect', () => {
+
+        console.log('user ' + chatID + ' disconnected');
+        socket.leave(chatID)
+    })
+
+    //Send message to only a particular user
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+
+        console.log('user ' + senderChatID + ' sent to ' + receiverChatID + "\n" + content);
+        //Send message to only that particular room
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID': receiverChatID,
+        })
+    })
 });
+
 http.listen(process.env.PORT)
