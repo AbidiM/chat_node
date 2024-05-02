42271f0ae97f15b3c86405b5cc4099ee2e93286e
AbidiM <mohamed-abidi@live.fr>
2024-05-02 12:36:21 +0100

    chat auth 0502242

--- index.js
+++ index.js
@@ -1,47 +1,132 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+app.use(express.json());
 
+const server = http.createServer(app);
 
-//rooms
-io.on('connection', socket => {
-    //Get the chatID of the user and join in a room of the same chatID
-    chatID = socket.handshake.query.chatID
+let io;
 
-    console.log('user ' + chatID + ' connected');
-    socket.join(chatID)
+const PORT = process.env.PORT || 3000;
+const writeRead = require('./routes/writeRead');
+const updateDelete = require('./routes/updateDelete');
+// const chat = require('./routes/chat');
 
-    //Leave the room if the user closes the socket
-    socket.on('disconnect', () => {
-
-        console.log('user ' + chatID + ' disconnected');
-        socket.leave(chatID)
-    })
-
-    //Send message to only a particular user
-    socket.on('send_message', message => {
-
-
-        msg = JSON.parse(message);
-
-
-        console.log(msg);
-        console.log(msg.receiverChatID);
-        console.log(msg.senderChatID);
-        console.log(msg.content);
-
-        receiverChatID = msg.receiverChatID
-        senderChatID = msg.senderChatID
-        content = msg.content
-
-        console.log('user ' + senderChatID + ' sent to ' + receiverChatID + "\n" + content);
-        //Send message to only that particular room
-        socket.in(receiverChatID).emit('receive_message', {
-            'content': content,
-            'senderChatID': senderChatID,
-            'receiverChatID': receiverChatID,
-        })
-    })
+app.use('/cr', writeRead);
+app.use('/ud', updateDelete);
+// app.use('/msg', chat);
+app.use('/', function (req, res, next) {
+    
+    console.log('api working');
+    res.sendStatus(404);
 });
 
-http.listen(process.env.PORT)
+app.route('/chat', (req, res) => {
+    // Check authorization logic here (e.g., using req.headers.token)
+    // if (/* authorized */) {
+    console.log('msg route working');
+    if (!io) { io = socketio(server) }
+    io.on('connection', (socket) => {
+        //Get the chatID of the user and join in a room of the same chatID
+        chatID = socket.handshake.query.chatID
+
+        console.log('user ' + chatID + ' connected');
+        socket.join(chatID)
+
+        //Leave the room if the user closes the socket
+        socket.on('disconnect', () => {
+
+            console.log('user ' + chatID + ' disconnected');
+            socket.leave(chatID)
+        })
+
+        //Send message to only a particular user
+        socket.on('send_message', message => {
+
+
+            msg = JSON.parse(message);
+
+
+            console.log(msg);
+            console.log(msg.receiverChatID);
+            console.log(msg.senderChatID);
+            console.log(msg.content);
+
+            receiverChatID = msg.receiverChatID
+            senderChatID = msg.senderChatID
+            content = msg.content
+
+            console.log('user ' + senderChatID + ' sent to ' + receiverChatID + "\n" + content);
+            //Send message to only that particular room
+            socket.in(receiverChatID).emit('receive_message', {
+                'content': content,
+                'senderChatID': senderChatID,
+                'receiverChatID': receiverChatID,
+            })
+        })
+    });
+    // } else {
+    //   res.status(401).send('Unauthorized');
+    // }
+});
+
+
+// app.listen(PORT, () =>
+//     console.log('Server running on port: ' + PORT
+//     ));
+
+server.listen(PORT, () =>
+    console.log('Server running on port: ' + PORT
+    ));
+
+
+
+
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+
+
+// //rooms
+// io.on('connection', socket => {
+//     //Get the chatID of the user and join in a room of the same chatID
+//     chatID = socket.handshake.query.chatID
+
+//     console.log('user ' + chatID + ' connected');
+//     socket.join(chatID)
+
+//     //Leave the room if the user closes the socket
+//     socket.on('disconnect', () => {
+
+//         console.log('user ' + chatID + ' disconnected');
+//         socket.leave(chatID)
+//     })
+
+//     //Send message to only a particular user
+//     socket.on('send_message', message => {
+
+
+//         msg = JSON.parse(message);
+
+
+//         console.log(msg);
+//         console.log(msg.receiverChatID);
+//         console.log(msg.senderChatID);
+//         console.log(msg.content);
+
+//         receiverChatID = msg.receiverChatID
+//         senderChatID = msg.senderChatID
+//         content = msg.content
+
+//         console.log('user ' + senderChatID + ' sent to ' + receiverChatID + "\n" + content);
+//         //Send message to only that particular room
+//         socket.in(receiverChatID).emit('receive_message', {
+//             'content': content,
+//             'senderChatID': senderChatID,
+//             'receiverChatID': receiverChatID,
+//         })
+//     })
+// });
+
+// http.listen(process.env.PORT)
